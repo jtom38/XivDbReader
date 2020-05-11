1d4c22ac5707a63eb6df7825b37f8e68bb788fb8
James Tombleson <luther38@gmail.com>
2020-05-10 18:01:32 -0700

    added dependancies to the setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="XivDbReader", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="James Tombleson",
     author_email="jtombleson38@gmail.com",
     description="Web scraping package to read the Final Fantasy XIV DB.",
@@ -18,5 +18,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ]
     python_requires='>=3.8',
 )
